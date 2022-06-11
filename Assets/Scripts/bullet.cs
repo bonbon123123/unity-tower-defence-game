c49a8d5d337551671fc8b7faf3d9977ab263e9d5
bonbon123123 <62888454+bonbon123123@users.noreply.github.com>
2022-06-11 03:32:17 +0200

    Towers start Shooting

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private Transform target;
+
+    public void Seek(Transform _target)
+    {
+        target= _target;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
