638c94b973577c0c29844c747cf63502732afe3b
bonbon123123 <62888454+bonbon123123@users.noreply.github.com>
2022-06-13 14:49:22 +0200

    Shooting is here
    Turrets shoot seeking bullets that 1 shot enemies and are destroyed on impact.

--- Assets/Scripts/bullet.cs
+++ Assets/Scripts/bullet.cs
@@ -4,21 +4,40 @@ public class Bullet : MonoBehaviour
 {
 
     private Transform target;
-
+    public GameObject destroyParticle;
+    public float speed = 70f;
     public void Seek(Transform _target)
     {
         target= _target;
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir=target.position-transform.position;
+        float distanceThisFrame=speed*Time.deltaTime;
+
+        if(dir.magnitude <= distanceThisFrame)
+        {
+            HitTarget();
+            return;
+        }
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+    }
+    void HitTarget()
+    {
+        GameObject effectIns = (GameObject)Instantiate(destroyParticle, transform.position, transform.rotation);
+        Destroy(effectIns, 2f);
+
+        Destroy(target.gameObject);
+        Destroy(gameObject);
+
     }
 }
